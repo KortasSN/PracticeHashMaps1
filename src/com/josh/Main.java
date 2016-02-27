a6b38cc886ba327719629772a2bfc304cfb13ba5
kortasthekiller <kortas@gmail.com>
2016-02-26 18:13:51 -0600

    Initial commit

---
+++ src/com/josh/Main.java
@@ -0,0 +1,14 @@
+package com.josh;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HashMap classes = new HashMap();
+        classes.put(2545, "Java Programming");
+        classes.put(1110, "Another Class");
+
+        System.out.println(classes);
+    }
+}
